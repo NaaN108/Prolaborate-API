0ced1866980cb1fc454485b60ecdac19aea1df01
NaaN108 <natha.paquette@sparxsystems.us>
2021-12-21 17:02:59 -0400

    emptied repo

--- utils/validations.js
+++
@@ -1,9 +0,0 @@
-var addUserValidate = [
-    check('emailAddress').isEmail().trim().escape().normalizeEmail(),
-    check('lastName').trim().escape(),
-    check('firstName').trim().escape()
-    ]
-
-module.exports = {
-    addUserValidate
-}
